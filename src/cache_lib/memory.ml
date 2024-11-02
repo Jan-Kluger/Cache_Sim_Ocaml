1409ecba260bd13d876e135f9fe0dc23c66fb024
yannick <yannick@schuermann.org>
2024-11-02 14:07:48 +0100

    analogous to cache, added module sig in the mli and a frame for module in the ml aslong as a record cacheline, used for easier keeping track of cacheline state

--- src/cache_lib/memory.ml
+++ src/cache_lib/memory.ml
@@ -1,1 +1,11 @@
-failwith "TODO"
+type cacheline = {
+  tag : int;
+  data : string;
+  valid : bool;
+}
+
+module Memory_int (R : Replacement_lib.Replacement_sig.REPLACEMENT_STRATEGY) : Memory_sig.MEMORY_SIG with type 'a t = int list = struct
+  type 'a t = int list
+  let read = failwith "todo"
+  let write = failwith "todo"
+end
