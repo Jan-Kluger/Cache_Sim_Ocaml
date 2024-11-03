cb04f8336cf60c7801b56562d3ce64bd554ef9b7
yannick <yannick@schuermann.org>
2024-11-03 18:42:04 +0100

    added logic to initialize the memory (excuse the type name)

--- src/cache_lib/memory.ml
+++ src/cache_lib/memory.ml
@@ -4,8 +4,27 @@ type cacheline = {
   valid : bool;
 }
 
-module Memory_int (R : Replacement_lib.Replacement_sig.REPLACEMENT_STRATEGY) : Memory_sig.MEMORY_SIG with type 'a t = int list = struct
-  type 'a t = int list
+type shithead = {
+  cache : cacheline array array;
+  associativity : int;
+}
+
+module Memory_int (R : Replacement_lib.Replacement_sig.REPLACEMENT_STRATEGY) : Memory_sig.MEMORY_SIG with type 'a t = shithead = struct
+  type 'a t = shithead
+
+  let create num_lines line_size associativity =
+    (* determin the number of set based on the number of lines and the associtivity *)
+    let num_sets = match associativity with
+      | 1 -> num_lines
+      | n when n = num_lines -> 1
+      | n -> num_lines / n
+    in
+    {
+      cache = Array.init num_sets (fun _ ->
+        Array.init line_size (fun _ -> { tag = -1; data = ""; valid = false })
+      );
+      associativity;
+    }
   let read = failwith "todo"
   let write = failwith "todo"
 end
